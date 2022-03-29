8b86fe39b637a05c3ce55ef7a1170e1a5b2f781a
phuongthao2145 <thaobtp@tdc.edu.vn>
2022-03-29 21:38:00 +0700

    v2.0

--- polls/forms.py
+++ polls/forms.py
@@ -2,6 +2,6 @@ from django.contrib.auth.forms import AuthenticationForm, UsernameField
 from django import forms
 
 class UploadFileForm(forms.Form):
-    file = forms.FileField(label="Chọn tệp")
+    file = forms.FileField(label="Chọn tệp",required=True)
 class ChangeFormForm(forms.Form):
-    file = forms.FileField(label="Chọn tệp")
+    file = forms.FileField(label="Chọn tệp",required=True)
